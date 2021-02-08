b1886c0597246500893559e3d5e243d60572cf02
davidabutler92 <davidabutler92@yahoo.com>
2021-02-08 13:36:02 -0800

    add fetched an array of 20 characters from avatar api

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import ListPage from '../ListPage/ListPage';
 import DetailPage from '../DetailPage/DetailPage';
+import { getCharacters } from '../services/avatarApi';
 
 export default function App() {
-  // const [characters, SetCharacters] = useState([]);
-  // useEffect(() => {
-  //   getCharacters().then((characters) => SetCharacters(characters));
-  // }, []);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getCharacters().then((characters) => setCharacters(characters));
+  }, []);
+
+  console.log(characters);
 
   return (
     <Router>
