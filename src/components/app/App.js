5ef4462fc6309bab8b49ddefa5fc8698f51d1099
davidabutler92 <davidabutler92@yahoo.com>
2021-02-08 13:25:50 -0800

    add routes to app.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,11 +5,16 @@ import AvatarContainer from '../Container/AvatarContainer';
 import DetailPage from '../DetailPage/DetailPage';
 
 export default function App() {
+  // const [characters, SetCharacters] = useState([]);
+  // useEffect(() => {
+  //   getCharacters().then((characters) => SetCharacters(characters));
+  // }, []);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={AvatarContainer} />
-        <Route exact path="/detail:id" component={DetailPage} />
+        <Route exact path="/detail" component={DetailPage} />
       </Switch>
     </Router>
   );
